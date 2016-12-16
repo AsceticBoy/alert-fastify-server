576b175f7c217aea0bd74af040f058d8ce58f5ab
AsceticBoy <398121373@qq.com>
2016-12-16 21:10:27 +0800

    chore: save User model

--- models/user.js
+++ models/user.js
@@ -7,3 +7,11 @@ var UserSchema = mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true }
 })
+
+// middleware before save
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+})
+
+module.exports = mongoose.model('User', UserSchema);
